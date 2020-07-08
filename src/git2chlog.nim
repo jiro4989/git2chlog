fbea0b2d3804eb38939c10574cf7b4c9add811ec
jiro4989 <jiroron666@gmail.com>
2020-07-08 16:22:51 +0000

    update

--- src/git2chlog.nim
+++ src/git2chlog.nim
@@ -1,5 +1,17 @@
 import osproc, strutils, strformat
 
+const
+  version = """codepoint version 1.0.0
+Copyright (c) 2020 jiro4989
+Released under the MIT License.
+https://github.com/jiro4989/git2chlog"""
+
+proc getAuthorName(): string =
+  discard
+
+proc getAuthorEmail(): string =
+  discard
+
 proc getLogs(startTag, endTag: string): seq[string] =
   let args = ["log", "--pretty=format:(%cn) %s", &"{startTag}..{endTag}"]
   result = execProcess("git", args = args, options = {poUsePath}).split("\n")
@@ -22,12 +34,15 @@ proc getTagSets(): seq[(string, string)] =
   for i in 0 ..< tags.len - 1:
     result.add((tags[i], tags[i+1]))
 
-iterator generateDebianChangeLog(pkg, version, author, email, datetime: string): string =
+iterator generateDebianChangeLog(pkg, author, email, datetime: string): string =
   let tagSets = getTagSets()
   for tagSet in tagSets:
-    let lines = getLogs(tagSet[0], tagSet[1]).formatLog(
+    let
+      startTag = tagSet[0]
+      endTag = tagSet[1]
+    let lines = getLogs(startTag, endTag).formatLog(
       pkg = pkg,
-      version = version,
+      version = startTag,
       author = author,
       email = email,
       datetime = datetime,
@@ -35,5 +50,12 @@ iterator generateDebianChangeLog(pkg, version, author, email, datetime: string):
     for line in lines:
       yield line
 
-when isMainModule and not defined modeTest:
+proc cmdDeb(pkg = "", author = "", email = "", datetime = ""): int =
   discard
+
+when isMainModule and not defined modeTest:
+  import cligen
+  clCfg.version = version
+  dispatchMulti(
+    [cmdDeb, cmdName = "deb"],
+  )
