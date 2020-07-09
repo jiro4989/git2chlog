52d65f67bcc9036d8605b76eda2776daac03724e
jiro4989 <jiroron666@gmail.com>
2020-07-09 13:13:26 +0000

    set no-pager

--- src/git2chlogpkg/git.nim
+++ src/git2chlogpkg/git.nim
@@ -3,18 +3,18 @@ from strutils import split, strip
 from strformat import `&`
 
 proc getAuthorName*(): string =
-  let args = ["log", "--format=%an", "--max-parents=0"]
+  let args = ["--no-pager", "log", "--format=%an", "--max-parents=0"]
   result = execProcess("git", args = args, options = {poUsePath}).strip
 
 proc getAuthorEmail*(): string =
-  let args = ["log", "--format=%ae", "--max-parents=0"]
+  let args = ["--no-pager", "log", "--format=%ae", "--max-parents=0"]
   result = execProcess("git", args = args, options = {poUsePath}).strip
 
 proc getLog*(startTag, endTag: string): seq[string] =
-  let args = ["log", "--pretty=format:(%cn) %s", &"{startTag}..{endTag}"]
+  let args = ["--no-pager", "log", "--pretty=format:(%cn) %s", &"{startTag}..{endTag}"]
   result = execProcess("git", args = args, options = {poUsePath}).split("\n")
 
 proc getTag*(): seq[string] =
-  let args = ["git", "tag", "-l", "--sort=-v:refname"]
+  let args = ["--no-pager", "tag", "-l", "--sort=-v:refname"]
   result = execProcess("git", args = args, options = {poUsePath}).split("\n")
 
