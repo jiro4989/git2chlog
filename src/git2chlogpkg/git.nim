3554fa03edda8a24fed64fc4b572e5619a9ed865
jiro4989 <jiroron666@gmail.com>
2020-07-09 17:08:44 +0000

    add author date

--- src/git2chlogpkg/git.nim
+++ src/git2chlogpkg/git.nim
@@ -10,6 +10,12 @@ proc getAuthorEmail*(): string =
   let args = ["--no-pager", "log", "--format=%ae", "--max-parents=0"]
   result = execProcess("git", args = args, options = {poUsePath}).strip
 
+proc getAuthorDate*(startTag, endTag: string): string =
+  let args = ["--no-pager", "log", "--format=%aD", &"{startTag}..{endTag}"]
+  let lines = execProcess("git", args = args, options = {poUsePath}).strip.split("\n")
+  if 1 <= lines.len:
+    result = lines[0]
+
 proc getLog*(startTag, endTag: string): seq[string] =
   let args = ["--no-pager", "log", "--pretty=format:(%cn) %s", &"{startTag}..{endTag}"]
   result = execProcess("git", args = args, options = {poUsePath}).strip.split("\n")
