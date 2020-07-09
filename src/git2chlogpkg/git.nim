00490d636c6690578f12e3541b6fdda1a3f728cf
jiro4989 <jiroron666@gmail.com>
2020-07-09 17:36:05 +0000

    args to const

--- src/git2chlogpkg/git.nim
+++ src/git2chlogpkg/git.nim
@@ -1,26 +1,36 @@
 import osproc
+from sequtils import concat
 from strutils import split, strip
 from strformat import `&`
 
+const
+  logArgs = @["--no-pager", "log"]
+  logArgsOneLine = concat(logArgs, @["--max-parents=0"])
+  logArgsAuthorName = concat(logArgsOneLine, @["--format=%an"])
+  logArgsAuthorEmail = concat(logArgsOneLine, @["--format=%ae"])
+  logArgsAuthorDate = concat(logArgs, @["--format=%aD"])
+  logArgsFormatLog = concat(logArgs, @["--pretty=format:(%cn) %s"])
+  tagArgs = @["--no-pager", "tag", "-l", "--sort=-v:refname"]
+
 proc getAuthorName*(): string =
-  let args = ["--no-pager", "log", "--format=%an", "--max-parents=0"]
-  result = execProcess("git", args = args, options = {poUsePath}).strip
+  result = execProcess("git", args = logArgsAuthorName, options = {poUsePath}).strip
 
 proc getAuthorEmail*(): string =
-  let args = ["--no-pager", "log", "--format=%ae", "--max-parents=0"]
-  result = execProcess("git", args = args, options = {poUsePath}).strip
+  result = execProcess("git", args = logArgsAuthorEmail, options = {poUsePath}).strip
 
 proc getAuthorDate*(startTag, endTag: string): string =
-  let args = ["--no-pager", "log", "--format=%aD", &"{startTag}..{endTag}"]
+  let args = concat(logArgsAuthorDate, @[&"{startTag}..{endTag}"])
   let lines = execProcess("git", args = args, options = {poUsePath}).strip.split("\n")
   if 1 <= lines.len:
     result = lines[0]
 
+proc getLog*(): seq[string] =
+  result = execProcess("git", args = logArgsFormatLog, options = {poUsePath}).strip.split("\n")
+
 proc getLog*(startTag, endTag: string): seq[string] =
-  let args = ["--no-pager", "log", "--pretty=format:(%cn) %s", &"{startTag}..{endTag}"]
+  let args = concat(logArgsFormatLog, @[&"{startTag}..{endTag}"])
   result = execProcess("git", args = args, options = {poUsePath}).strip.split("\n")
 
 proc getTag*(): seq[string] =
-  let args = ["--no-pager", "tag", "-l", "--sort=-v:refname"]
-  result = execProcess("git", args = args, options = {poUsePath}).strip.split("\n")
+  result = execProcess("git", args = tagArgs, options = {poUsePath}).strip.split("\n")
 
